55a804d9a59da1f94c82849d89574f0518dc12a9
joavega21 <isnp151323@ugb.edu.sv>
2024-04-19 23:14:49 -0700

    Programa muestra suma de 5 valores ingresados por usuario

---
+++ Ejercicio4/Ejercicio4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4 {
+    class Program {
+        static void Main(string[] args) {
+            int num = 0, suma = 0, i = 0;
+            Console.WriteLine("Ingrese los valores a sumar : ");
+            do {
+                num = int.Parse(Console.ReadLine());
+                suma = suma + num;
+            } while (i < 5);
+            Console.WriteLine("El total de la suma es: {0} ", suma);
+            Console.ReadKey();
+        }
+    }
+}
