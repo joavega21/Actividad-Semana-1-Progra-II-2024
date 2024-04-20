452c3d69234809435247b4b71707f23e72c898b2
joavega21 <isnp151323@ugb.edu.sv>
2024-04-19 22:34:27 -0700

    Programa que muestra numeros del 1 al 10 usando While

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2 {
+    class Program {
+        static void Main(string[] args) {
+            int num = 1;
+            while(num <= 10) {
+                Console.WriteLine("{0}");
+                num++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
