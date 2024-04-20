f4bce15f687c7da9667ad720e814c93aca03c4d6
joavega21 <isnp151323@ugb.edu.sv>
2024-04-20 00:01:18 -0700

    asd

--- Ejercicio5/Ejercicio5/Program.cs
+++ Ejercicio5/Ejercicio5/Program.cs
@@ -7,19 +7,18 @@ using System.Threading.Tasks;
 namespace Ejercicio5 {
     class Program {
         static void Main(string[] args) {
-            int i = 0;
-            double promedio = 0, suma = 0;
+            int promedio = 0, suma = 0, i = 0;
             Boolean continuar = true;
-            Console.WriteLine("Ingrese valores positivos para promediar, o ingrese cero para salir: ");
+            Console.WriteLine("Ingrese un valor positivo para promediar, o ingrese cero para salir: ");
             while (continuar) {
-                double num = double.Parse(Console.ReadLine());
+                int num = int.Parse(Console.ReadLine());
                 suma = suma + num;
                 i++;
-             }
-            continuar = !Console.ReadLine().Equals("0");
+                continuar = !Console.ReadLine().Equals("0");
+            }
             promedio = suma / i;
+
             Console.WriteLine("El promedio de los valores ingresados es: {0}", promedio);
-            Console.ReadKey();
         }
     }
 }
