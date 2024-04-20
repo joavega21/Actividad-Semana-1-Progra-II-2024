dffa37117cb7c082f24353e7d54e6af8b4b92eba
joavega21 <isnp151323@ugb.edu.sv>
2024-04-19 23:34:13 -0700

    El programa muestra el promedio de valores ingresados por usuario

---
+++ Ejercicio5/Ejercicio5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5 {
+    class Program {
+        static void Main(string[] args) {
+            int promedio = 0, suma = 0, i = 0;
+            Console.WriteLine("Ingrese un valor positivo para promediar, o ingrese cero para salir: ");
+            int num = int.Parse(Console.ReadLine());
+            while (num > 0) {
+                suma = suma + num;
+                i++;
+            }
+            promedio = suma / i;
+            Console.WriteLine("El promedio de los valores ingresados es: {0}", promedio);
+            Console.ReadKey();
+        }
+    }
+}
