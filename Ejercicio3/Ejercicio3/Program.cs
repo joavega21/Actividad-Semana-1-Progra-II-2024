15480e1f0a1b6498c55cff9ecf5964f8b57d284f
joavega21 <isnp151323@ugb.edu.sv>
2024-04-19 22:58:34 -0700

    Programa muestra la suma de valores ingresados por usuario.

--- Ejercicio3/Ejercicio3/Program.cs
+++ Ejercicio3/Ejercicio3/Program.cs
@@ -8,8 +8,8 @@ namespace Ejercicio3 {
     class Program {
         static void Main(string[] args) {
             int num = 0, suma = 0;
+            Console.WriteLine("Ingrese un numero positivo para sumar: ");
             do {
-                Console.WriteLine("Ingrese un numero positivo para sumar: ");
                 num = int.Parse(Console.ReadLine());
                 suma = suma + num;
                 Console.WriteLine("El total de la suma es: {0} ", suma);
